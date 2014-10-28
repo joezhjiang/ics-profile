652c7fa52369b692a8b3000f1c72ea42a0b7ac56
joezhjiang <joe.zhjiang@gmail.com>
2014-10-28 11:15:02 +0800

    initial

---
+++ src/main/java/org/ics/profile/App.java
@@ -0,0 +1,13 @@
+package org.ics.profile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
